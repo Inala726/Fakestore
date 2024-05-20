95dc3583a2e670212875d3903821b233c0e69bd1
Inala <inalajacob740@gmail.com>
2024-05-20 12:34:39 +0100

    Just concluded design

--- Ecommerce/src/components/Ecommerce.tsx
+++ Ecommerce/src/components/Ecommerce.tsx
@@ -1,10 +1,13 @@
 import { FaSearch } from 'react-icons/fa';
 import './ecommerce.css';
 import { FaCartShopping, FaPerson } from 'react-icons/fa6';
+// import picture from '../assets/images/wallpaperflare.com_wallpaper (1).jpg'
 
 // import React from 'react'
 
 const Ecommerce = () => {
+
+  
   return (
     <>
         <nav className="navbar">
@@ -13,15 +16,25 @@ const Ecommerce = () => {
             </div>
             <div className="">
               <div className="search">
-                <input type="text" />
+                <input type="text" placeholder='Search products...' />
                 <FaSearch/>
               </div>
             </div>
-            <div className="">
-              <FaCartShopping/>
-              <FaPerson/>
+            <div className="third">
+              <FaCartShopping size={25}/>
+              <FaPerson size={20}/>
             </div>
         </nav>
+        <div className="section">
+          <p>Try out some</p>
+          <p>of our splendid <br /> Products</p>
+        </div>
+        <div className="heading">
+          <h1>Available Products</h1>
+        </div>
+        <div className="">
+
+        </div>
     </>
   )
 }
